09b5a79482bab777c79b2d6e642ce5291cedb5a8
doug-cady <douglas.cady@posteo.net>
2021-07-20 14:15:30 -0500

    updated part 2 prog with better defined functions, completed part 2

--- Misc/python/advent_of_code/2016/day_09/bin/advent_2016_day9_pt2.py
+++ Misc/python/advent_of_code/2016/day_09/bin/advent_2016_day9_pt2.py
@@ -13,12 +13,13 @@ from typing import List, Tuple
 
 
 tests = {
-    # 'ADVENT': 'ADVENT',
-    # '(3x3)XYZ': 'XYZXYZXYZ',
-    # 'A(1x5)BC': 'ABBBBBC',
-    # 'A(2x2)BCD(2x2)EFG': 'ABCBCDEFEFG',
-    # '(6x1)(1x3)A': 'AAA',
-    # 'X(8x2)(3x3)ABCY': 'XABCABCABCABCABCABCY',
+    'ADVENT': 'ADVENT',
+    '(3x3)XYZ': 'XYZXYZXYZ',
+    'A(1x5)BC': 'ABBBBBC',
+    'A(2x2)BCD(2x2)EFG': 'ABCBCDEFEFG',
+    '(6x1)(1x3)A': 'AAA',
+    'X(8x2)(3x3)ABCY': 'XABCABCABCABCABCABCY',
+    '(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN': 445,
     '(27x12)(20x12)(13x14)(7x10)(1x12)A': 241920
 }
 
@@ -31,68 +32,44 @@ def test_parse_instructions() -> None:
             assert parse_instructions(instr) == len(ans)
 
 
-def decompress_marker(rem_proc: str) -> Tuple[int, int]:
-    """Process marker like (3x2) to repeat next 3 characters 2 times.
-        > Returns the length of repeated characters and the index skip value
-    """
-    skip_2_paren_lens = 2
+def decompress(rem_proc: str) -> Tuple[str, int]:
+    """Decompress message starting at identified marker and return decompressed message."""
     idx_add = 0
-    rept_msg = rem_proc
-    marker_len = 0
-    print(f"\nMarker init: {rept_msg}")
+    skip_2_paren_len = 2
+    btw_parens_len = rem_proc[1:].index(')')
 
-    while True:
-        rept_next_n, rest = rept_msg[1:].split('x', maxsplit=1)
-        rept_next_n_int = int(rept_next_n)
-        rept_num, rest = rest.split(')', maxsplit=1)
-        rept_chars = rest[:rept_next_n_int]
-        idx_add += skip_2_paren_lens + rept_msg[1:].index(')')
-        rept_msg = ''.join([rept_chars] * int(rept_num))
+    rept_next_n, rest = rem_proc[1:].split('x', maxsplit=1)
+    rept_next_n_int = int(rept_next_n)
+    rept_n_times, rest = rest.split(')', maxsplit=1)
+    rept_chars = rest[:rept_next_n_int]
+    idx_add += skip_2_paren_len + btw_parens_len
 
-        print(f"\nrept_next_n: {rept_next_n} | rept_num: {rept_num} | rept_chars: {rept_chars}")
-        print(f"rest: {rest} | rept_msg: {rept_msg} | idx_add: {idx_add}")
+    rept_msg = ''.join([rept_chars] * int(rept_n_times))
+    after_marker_msg = rem_proc[idx_add + rept_next_n_int:]
 
-        if '(' not in rept_chars:
-            marker_len += len(rept_msg)
-
-            if ('(' not in rest) | (len(rest) == rept_next_n_int):
-                idx_add += rept_next_n_int
-                break
-            elif rest[rept_next_n_int] == '(':
-                rept_msg = rest[rept_next_n_int:]
-                idx_add -= skip_2_paren_lens + rept_msg[1:].index(')')
-                print(f"- rept_msg: {rept_msg}")
-            else:
-                idx_add += rept_next_n_int
-                break
-
-        print(f"marker_len: {marker_len}")
-
-    print(f"marker_len: {marker_len}")
-    return marker_len, idx_add
+    if '(' in rept_msg:
+        return rept_msg + after_marker_msg, 0
+    else:
+        return after_marker_msg, len(rept_msg)
 
 
 def parse_instructions(proc: str) -> str:
-    """Parse puzzle input and identify repeat markers to send to marker function."""
-    decomp_msg = ''
+    """Parse puzzle input and identify repeat markers to send to decompress function."""
+    msg = proc
     idx = 0
-    decomp_len = 0
-    print("\n---------------------------------------------------------")
-    print("proc:", proc)
-    print("len proc:", len(proc))
+    final_len = 0
 
-    while(idx < len(proc)):
-        if proc[idx] == '(':
-            len_rept_chars, idx_add = decompress_marker(rem_proc=proc[idx:])
-            decomp_len += len_rept_chars
-            idx += idx_add
-            print("idx_add:", idx_add, "idx:", idx)
+    while len(msg) > 0:
+        if msg[idx] == '(':
+            msg, decomp_len = decompress(rem_proc=msg[idx:])
+            final_len += decomp_len
+            idx = 0
         else:
-            decomp_len += 1
-            idx += 1
+            final_len += 1impo
+            msg = msg[1:]
 
-    print("decomp_len:", decomp_len)
-    return decomp_len
+    print("final_len:", final_len)
+    return final_len
 
 
 
@@ -101,11 +78,11 @@ def main() -> int:
     decomp_msg = test_parse_instructions()
 
     # Final puzzle input
-    # with open("../data/puz_input_2016_d9_pt1.txt", 'r') as f:
-    #     input_proc = f.read().strip(' ').strip('\n')
+    with open("../data/puz_input_2016_d9_pt1.txt", 'r') as f:
+        input_proc = f.read().strip(' ').strip('\n')
 
-    # decomp_msg = parse_instructions(proc=input_proc)
-    # print(f"Length: {len(decomp_msg):,}")
+    decomp_len = parse_instructions(proc=input_proc)
+    print(f"Length: {decomp_len:,}")
 
     return 0
 
