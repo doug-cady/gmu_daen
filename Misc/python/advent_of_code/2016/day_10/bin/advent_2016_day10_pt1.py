d657217d16d96eef26eb467f06073b59dfb2c460
doug-cady <douglas.cady@posteo.net>
2021-07-21 21:19:05 -0500

    learned about difference between string and integer comparison in this program during debugging. completed day 10 problem parts 1 and 2

--- Misc/python/advent_of_code/2016/day_10/bin/advent_2016_day10_pt1.py
+++ Misc/python/advent_of_code/2016/day_10/bin/advent_2016_day10_pt1.py
@@ -9,8 +9,11 @@ url: https://adventofcode.com/2016/day/10
 import pytest
 import sys
 import numpy as np
+from math import prod
 from collections import defaultdict
-from typing import List, DefaultDict
+from copy import deepcopy
+from itertools import cycle
+from typing import List, DefaultDict, Tuple
 
 
 test_input = """value 5 goes to bot 2
@@ -22,54 +25,45 @@ value 2 goes to bot 2"""
 
 
 def test_init_bots() -> None:
-    bot_test = BotBalance(instrs=test_input.split('\n'))
+    bot_test = BotBalance(instrs=test_input.split('\n'), comp_2_chips=[])
     obs_dict = bot_test.in_bots
 
     exp_dict = defaultdict(list)
-    exp_dict['2'].append('5')
-    exp_dict['1'].append('3')
-    exp_dict['2'].append('2')
+    exp_dict['2'].append(5)
+    exp_dict['1'].append(3)
+    exp_dict['2'].append(2)
 
     assert obs_dict == exp_dict
 
 
 def test_parse_input() -> None:
-    bot_test = BotBalance(instrs=test_input.split('\n'))
+    bot_test = BotBalance(instrs=test_input.split('\n'), comp_2_chips=[])
     bot_test.move()
     obs_moved_bots = bot_test.move_bots
-    obs_bots_archive = bot_test.bots_archive
     obs_output = bot_test.output
 
     exp_moved_bots = defaultdict(list)
-    print(obs_moved_bots)
+    exp_moved_bots['2'] = []
+    exp_moved_bots['1'] = []
+    exp_moved_bots['0'] = []
     assert obs_moved_bots == exp_moved_bots
 
-    exp_bots_archive = defaultdict(list)
-    exp_bots_archive['2'].append('5')
-    exp_bots_archive['1'].append('3')
-    exp_bots_archive['2'].append('2')
-    exp_bots_archive['1'].append('2')
-    exp_bots_archive['0'].append('5')
-    exp_bots_archive['0'].append('3')
-    assert obs_bots_archive == exp_bots_archive
-
     exp_output = defaultdict(list)
-    exp_output['1'].append('2')
-    exp_output['2'].append('3')
-    exp_output['0'].append('5')
+    exp_output['1'].append(2)
+    exp_output['2'].append(3)
+    exp_output['0'].append(5)
     assert obs_output == exp_output
 
 
-
 class BotBalance:
-    def __init__(self, instrs: List[str]):
+    def __init__(self, instrs: List[str], comp_2_chips: List[str]):
         self.instrs = instrs
         self.init_instrs = self.extract_instrs_by_type(instr_type='val')
         self.move_instrs = self.extract_instrs_by_type(instr_type='bot')
         self.in_bots = self.init_bots()
-        self.bots_archive = self.in_bots
-        self.move_bots = self.in_bots
+        self.move_bots = deepcopy(self.in_bots)
         self.output = defaultdict(list)
+        self.comp_2_chips = comp_2_chips
 
 
     def extract_instrs_by_type(self, instr_type: str) -> List[str]:
@@ -85,43 +79,63 @@ class BotBalance:
         for instr in self.init_instrs:
             """value [5] goes to bot [2]"""
             _, val, _, _, _, bot = instr.split(' ')
-            bots[bot].append(val)
+            bots[bot].append(int(val))
 
         return bots
 
 
-    def assign_values(self, val: str, rec: str, rec_num: str) -> None:
+    def assign_values(self, val: int, rec: str, rec_num: str) -> None:
         """Assign val to the recipient "rec" with rec_num and return updated dictionaries."""
-        print(f"gives val {val} to {rec} {rec_num}")
-
         if rec == 'bot':
             self.move_bots[rec_num].append(val)
-            self.bots_archive[rec_num].append(va)
         else:
             self.output[rec_num].append(val)
 
 
+    def bot_has_2_chips(self, bot: str) -> bool:
+        if len(self.move_bots.get(bot, [])) == 2:
+            return True
+        else:
+            return False
+
+
     def move(self) -> None:
         """Move bots according to move instructions and return archive of bots with max values."""
-        output = defaultdict(list)
+        used_moves = set()
+        self.compare_bots = set()
+        idx = 0
 
-        for instr in instrs:
-            """bot [2] gives [low] to [bot] [1] and [high] to [bot] [0]"""
-            _, giver, _, _, _, lo_rec, lo_rec_num, _, _, _, hi_rec, hi_rec_num = instr.split(' ')
-            lo, hi = sorted(self.move_bots[giver])
-            self.move_bots[giver] = [] # bot is empty now
+        while len(self.move_instrs) != len(used_moves):
+            instr = self.move_instrs[idx]
+            idx += 1
 
-            self.assign_values(val=lo, rec=lo_rec, rec_num=lo_rec_num)
-            self.assign_values(val=hi, rec=hi_rec, rec_num=hi_rec_num)
+            if instr not in used_moves:
+                """bot [2] gives [low] to [bot] [1] and [high] to [bot] [0]"""
+                _, giver, _, _, _, lo_rec, lo_rec_num, _, _, _, hi_rec, hi_rec_num = instr.split(' ')
+
+                if self.bot_has_2_chips(bot=giver):
+                    idx = 0
+                    used_moves.add(instr)
+                    lo, hi = sorted(self.move_bots[giver])
+
+                    if [hi, lo] == self.comp_2_chips:
+                        self.bot_compare_ans = giver
+                        # break
+
+                    self.move_bots[giver] = [] # giver bot is emptied
+
+                    self.assign_values(val=lo, rec=lo_rec, rec_num=lo_rec_num)
+                    self.assign_values(val=hi, rec=hi_rec, rec_num=hi_rec_num)
 
 
-def parse_input(puz_input: List[str]) -> int:
+
+def parse_input(puz_input: List[str], comp_2_chips: List[int]) -> Tuple[str, int]:
     """Parse puzzle input, then split value and move instructions."""
-    bot_test = BotBalance(instrs=test_input)
+    bot_bal_input = BotBalance(instrs=puz_input, comp_2_chips=comp_2_chips)
+    bot_bal_input.move()
+    output_012 = prod(sum([bot_bal_input.output[num] for num in ['0', '1', '2']], []))
+    return bot_bal_input.bot_compare_ans, output_012
 
-    bots = init_bots(instrs=init_value_instrs)
-
-    bot_value_compares = move(in_bots=bots, instrs=move_instrs)
 
 
 # Main ------------------------------------------------------------------------
@@ -130,11 +144,14 @@ def main() -> int:
     test_parse_input()
 
     # Final puzzle input
-    # with open("../data/puz_input_2016_d9_pt1.txt", 'r') as f:
-    #     input_proc = f.read().split('\n')
+    with open("../data/puz_input_2016_d10_pt1.txt", 'r') as f:
+        puz_input = [line for line in f.read().split('\n') if line != '']
 
-    # decomp_len = parse_instructions(proc=input_proc)
-    # print(f"Length: {decomp_len:,}")
+    comp_2_chips = [61, 17]
+    bot_compare_ans, output_012_mult = parse_input(puz_input=puz_input, comp_2_chips=comp_2_chips)
+    print(f"[Part 1] - Bot {bot_compare_ans} compares the {comp_2_chips} microchips.")
+
+    print(f"[Part 2] - Output012 multiplied = {output_012_mult}")
 
     return 0
 
