841a94a16c1578bd3a9b3cfd9d33ab84d5f34310
doug-cady <douglas.cady@posteo.net>
2021-11-21 15:27:26 -0600

    Began work on info viz portion of project. created final versions of a few visualizations and stored as pngs in graphics folder

---
+++ AIT664_InfoReprProcVisuals/project/bin/ggplot_theme.R
@@ -0,0 +1,29 @@
+
+ggplot_clean_theme <- theme_gray()+ theme(
+  plot.title=element_text(hjust=0.5, size = 16),
+  plot.subtitle=element_text(hjust=0.5, size = 14, face = 'italic'),
+  # plot.caption=element_text(hjust=-.5),
+
+  # strip.text.y = element_blank(),
+  strip.background=element_rect(fill=rgb(.9,.95,1),
+    colour=gray(.5), size=.2),
+
+  panel.border=element_rect(fill=FALSE,colour=gray(.70)),
+  panel.grid.minor.y = element_blank(),
+  panel.grid.minor.x = element_blank(),
+  panel.spacing.x = unit(0.10, "cm"),
+  panel.spacing.y = unit(0.10, "cm"),
+
+# axis.ticks.y= element_blank()
+  axis.ticks=element_blank(),
+  axis.text=element_text(colour="black"),
+  axis.text.y=element_text(margin=ggplot2::margin(0,3,0,3)),
+  axis.text.x=element_text(margin=ggplot2::margin(-1,0,3,0)),
+
+  # Specific to HFI, suicide row-labeled plots
+  text = element_text(size = 12),
+  strip.text = element_blank(),
+  legend.position = "none"
+)
+
+
